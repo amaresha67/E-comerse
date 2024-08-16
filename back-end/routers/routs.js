355d0b72934436f88495b1dd54274ff6c3f4d3e5
Amaresha <amaresha1a2b@gmail.com>
2024-08-16 21:32:48 +0530

    first commite

--- back-end/routers/routs.js
+++ back-end/routers/routs.js
@@ -23,16 +23,16 @@ router.post("/admin/update", Update);
 router.post("/admin/getprofile", userdata);
 router.post("/admin/loginvalidate", loginValidation);
 
+router.post("/product/addproduct", addProduct);
+router.get("/product/getproductsdata", getProductsData);
+router.post("/product/updateproduct", updateProductsData);
+router.post("/product/deleteproduct", deleteProduct);
+
 router.post("/user/saveUser", saveUserData);
 router.post("/user/userLoginValidate", UserloginValidation);
 router.post("/user/getProductsDataByuser", getProductsDataByuser);
 router.post("/user/getUserId", getUserId);
 router.post("/user/getuserdata", getUserData);
-
-router.post("/product/addproduct", addProduct);
-router.get("/product/getproductsdata", getProductsData);
-router.post("/product/updateproduct", updateProductsData);
-router.post("/product/deleteproduct", deleteProduct);
 
 router.post("/cart/addtocart", addToCart);
 router.post("/cart/cartitems", cartItems);
